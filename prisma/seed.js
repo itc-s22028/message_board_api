cbb66578921e4f60f37d9f29a2e9091b8891a44f
itc-s22028 <s22028@std.it-college.ac.jp>
2024-01-31 16:31:47 +0900

    pugを使ってLogin画面を表示させた

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,6 +1,7 @@
 const {PrismaClient} = require("@prisma/client");
 const scrypt = require("../util/scrypt");
 const prisma = new PrismaClient();
+
 const main = async () => {
     let salt;
     // 1人目
@@ -23,6 +24,8 @@ const main = async () => {
             name: "hanako",
             password: scrypt.calcHash("flower", salt),
             salt,
+            email: "hanako@flower",
+            age: 28
         }
     });
     // 3人目
@@ -34,6 +37,8 @@ const main = async () => {
             name: "sachiko",
             password: scrypt.calcHash("happy", salt),
             salt,
+            email: "sachiko@happy",
+            age: 17
         }
     });
     // 4人目
@@ -45,6 +50,8 @@ const main = async () => {
             name: "jiro",
             password: scrypt.calcHash("change", salt),
             salt,
+            email: "jiro@change",
+            age: 6
         }
     });
     // 5人目
@@ -56,6 +63,8 @@ const main = async () => {
             name: "mami",
             password: scrypt.calcHash("mumemo", salt),
             salt,
+            email: "mami@mumemo",
+            age: 41
         }
     });
     // 6人目
@@ -67,6 +76,8 @@ const main = async () => {
             name: "ichiro",
             password: scrypt.calcHash("baseball", salt),
             salt,
+            email: "ichiro@base.ball",
+            age: 52
         }
     });
     // 7人目
@@ -78,8 +89,19 @@ const main = async () => {
             name: "kumi",
             password: scrypt.calcHash("co", salt),
             salt,
+            email: "kumi@co",
+            age: 63
         }
     });
 };
 
 main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
+
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const router = require("express").Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/login", (req, res, next) => {
+  const data = {
+    title: "Users/Login",
+    content: "名前とパスワードを入力ください"
+  };
+  res.render("users/login", data);
 });
 
 module.exports = router;
